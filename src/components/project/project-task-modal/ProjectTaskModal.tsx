6ea284ae65211909531fad4715a1b55756376492
Joaquin Barrandeguy <joaquinbarrandeguy@gmail.com>
2024-02-13 18:29:07 -0300

    task modal scroll bug 4.0

--- src/components/project/project-task-modal/ProjectTaskModal.tsx
+++ src/components/project/project-task-modal/ProjectTaskModal.tsx
@@ -21,8 +21,8 @@ export const ProjectTaskModal = ({ projectId }: Props) => {
     };
 
     const modalClasses = isTaskModalOpen
-        ? 'fixed top-0 right-0 bottom-0 flex flex-col w-full lg:w-1/3 h-full transition-right-left duration-300 transform translate-x-0 bg-white z-30'
-        : 'fixed top-0 left-full bottom-0 flex flex-col w-full lg:w-1/3 h-full transition-right-left duration-300 transform translate-x-full bg-white';
+        ? 'fixed top-0 right-0 bottom-0 flex flex-col w-full lg:w-1/3 h-screen transition-right-left duration-300 transform translate-x-0 bg-white z-30'
+        : 'fixed top-0 left-full bottom-0 flex flex-col w-full lg:w-1/3 h-screen transition-right-left duration-300 transform translate-x-full bg-white';
 
     return (
         <>
@@ -34,7 +34,7 @@ export const ProjectTaskModal = ({ projectId }: Props) => {
             >
                 <ProjectSubTaskModal />
 
-                <div className="p-4 md:p-5 border-l dark:border-gray-600 h-screen">
+                <div className="p-4 md:p-5 border-l dark:border-gray-600 h-full">
 
                     {
                         taskModalType === ModalType.New
--- src/components/project/project-task-modal/ProjectTaskModalAddNewTask.tsx
+++ src/components/project/project-task-modal/ProjectTaskModalAddNewTask.tsx
@@ -7,6 +7,8 @@ import clsx from "clsx";
 import { useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import Datepicker from "react-tailwindcss-datepicker";
+import { FaSave } from 'react-icons/fa';
+import { FaX } from 'react-icons/fa6';
 
 
 interface Props {
@@ -73,39 +75,37 @@ export const ProjectTaskModalAddNewTask = ({ projectId }: Props) => {
     return (
 
 
-        <div className="relative h-screen max-h-full overflow-y-auto">
-            <div className="flex items-center justify-between py-4 border-b rounded-t dark:border-gray-600">
-                <h3 className="text-lg font-normal text-gray-900">
-                    New <span className="font-extrabold">Task</span>
-                </h3>
-                <button
-                    type="button"
-                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-toggle="crud-modal"
-                    onClick={closeTaskModal}
-                >
-                    <svg
-                        className="w-3 h-3"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 14 14"
-                    >
-                        <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                        />
-                    </svg>
-                    <span className="sr-only">Close modal</span>
-                </button>
-            </div>
-
-            <div className="grid gap-4 mb-4 grid-cols-2 "></div>
-
+        <div className="relative h-full max-h-full overflow-y-auto">
             <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 md:space-y-6">
+                <div className="flex items-center justify-between py-4 border-b rounded-t dark:border-gray-600">
+                    <h3 className="text-lg font-normal text-gray-900">
+                        New <span className="font-extrabold">Task</span>
+                    </h3>
+                    <div className="flex">
+                        <div className="flex justify-end space-x-1">
+                            <button
+                                type="submit"
+                                className={`bg-transparent rounded-lg text-lg w-6 h-6 inline-flex justify-center items-center text-sky-700`}
+                            >
+                                <FaSave />
+                                <span className="sr-only">Save</span>
+                            </button>
+                            <button
+                                type="button"
+                                className="text-red-700 bg-transparent rounded-lg text-lg w-6 h-6 inline-flex justify-center items-center"
+                                data-modal-toggle="crud-modal"
+                                onClick={closeTaskModal}
+                            >
+                                <FaX />
+                                <span className="sr-only">Close modal</span>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="grid gap-4 mb-4 grid-cols-2 "></div>
+
 
                 <div>
                     <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Task Name</label>
@@ -115,7 +115,7 @@ export const ProjectTaskModalAddNewTask = ({ projectId }: Props) => {
                         placeholder="Task 01"
                         className={
                             clsx(
-                                "bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block w-screen h-10 p-2.5",
+                                "bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block w-full h-10 p-2.5",
                                 {
                                     'focus:outline-none focus:border-2 border-pink-500 text-pink-600 focus:border-pink-500 focus:ring-pink-500': !!errors.name
                                 }
@@ -136,7 +136,7 @@ export const ProjectTaskModalAddNewTask = ({ projectId }: Props) => {
                         placeholder="Kitchen, bedroom ..."
                         className={
                             clsx(
-                                "bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block w-screen h-10 p-2.5",
+                                "bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block w-full h-10 p-2.5",
                                 {
                                     'focus:outline-none focus:border-2 border-pink-500 text-pink-600 focus:border-pink-500 focus:ring-pink-500': !!errors.name
                                 }
@@ -157,7 +157,7 @@ export const ProjectTaskModalAddNewTask = ({ projectId }: Props) => {
                         placeholder="Contractor name"
                         className={
                             clsx(
-                                "bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block w-screen h-10 p-2.5",
+                                "bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block w-full h-10 p-2.5",
                                 {
                                     'focus:outline-none focus:border-2 border-pink-500 text-pink-600 focus:border-pink-500 focus:ring-pink-500': !!errors.name
                                 }
@@ -175,7 +175,7 @@ export const ProjectTaskModalAddNewTask = ({ projectId }: Props) => {
                     <label htmlFor="start-end" className="block mb-2 text-sm font-medium text-gray-900">Start and End date</label>
                     <div className="relative">
                         <Datepicker
-                            inputClassName="bg-gray-50 border text-gray-900 sm:text-sm rounded-lg  w-screen h-10 p-2.5"
+                            inputClassName="bg-gray-50 border text-gray-900 sm:text-sm rounded-lg  w-full h-10 p-2.5"
                             placeholder={"Select Dates"}
                             value={value}
                             onChange={handleValueChange}
@@ -190,7 +190,7 @@ export const ProjectTaskModalAddNewTask = ({ projectId }: Props) => {
                         id="priority"
                         className={
                             clsx(
-                                "bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block w-screen h-10 p-2.5",
+                                "bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block w-full h-10 p-2.5",
                                 {
                                     'focus:outline-none focus:border-2 border-pink-500 text-pink-600 focus:border-pink-500 focus:ring-pink-500': !!errors.name
                                 }
@@ -213,7 +213,7 @@ export const ProjectTaskModalAddNewTask = ({ projectId }: Props) => {
                         id="status"
                         className={
                             clsx(
-                                "bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block w-screen h-10 p-2.5",
+                                "bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block w-full h-10 p-2.5",
                                 {
                                     'focus:outline-none focus:border-2 border-pink-500 text-pink-600 focus:border-pink-500 focus:ring-pink-500': !!errors.name
                                 }
@@ -230,25 +230,16 @@ export const ProjectTaskModalAddNewTask = ({ projectId }: Props) => {
                     </select>
                 </div>
 
-
                 <div>
                     <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900">Project description</label>
                     <textarea
                         rows={3}
                         id="description"
                         placeholder="Description..."
-                        className="bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block p-2.5 w-screen text-sm "
+                        className="bg-gray-50 border text-gray-900 sm:text-sm rounded-lg block p-2.5 w-full text-sm "
                         {...register('description')}
                     ></textarea>
                 </div>
-
-
-                <button
-                    type="submit"
-                    className="btn-login mb-4"
-                >
-                    Add
-                </button>
             </form>
         </div>
 
