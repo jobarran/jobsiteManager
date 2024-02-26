165d28ee134c2bfb8bb11ec66dc6a10269715598
Joaquin Barrandeguy <joaquinbarrandeguy@gmail.com>
2024-02-26 18:47:46 -0300

    add project new attributes

--- src/app/(jobsite-management)/project/[id]/doc/page.tsx
+++ src/app/(jobsite-management)/project/[id]/doc/page.tsx
@@ -15,16 +15,16 @@ export default async function ProjectByIdDocsPage({ params }: Props) {
 
     const breadElements = [
         {
-          name: 'Projects',
-          href: '/project',
-          link: '/project',
+            name: 'Projects',
+            href: '/project',
+            link: '/project',
         },
         {
             name: 'Docs',
             href: '',
             link: '',
-          },
-      ]
+        },
+    ]
 
     return (
         <div>
@@ -38,6 +38,9 @@ export default async function ProjectByIdDocsPage({ params }: Props) {
                         name={project.name}
                         description={project.description}
                         status={project.status}
+                        shortName={project.shortName ? project.shortName : ''}
+                        location={project.location ? project.location : ''}
+                        end={project.end ? project.end : ''}
                     />
                     : <div></div>
             }
