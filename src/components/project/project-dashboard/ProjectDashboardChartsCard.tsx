461b67d7e65791c7cd42ace89e1dba7ede29c629
Joaquin Barrandeguy <joaquinbarrandeguy@gmail.com>
2024-02-26 18:42:45 -0300

    DEBUG

--- src/components/project/project-dashboard/ProjectDashboardChartsCard.tsx
+++ src/components/project/project-dashboard/ProjectDashboardChartsCard.tsx
@@ -6,7 +6,7 @@ export const ProjectDashboardChartsCard = () => {
     return (
 
         <div className="w-full p-4 border border-gray-100 bg-white rounded-lg sm:p-8">
-            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">Charts</h5>
+            <h5 className="mb-4 text-xl font-medium text-gray-500 dark:text-gray-400">ChartSs</h5>
             
             
             
